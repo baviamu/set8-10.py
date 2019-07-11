e12d152ecc532c39d1e29ba35c56afd7d5e3a979
baviamu <48151961+baviamu@users.noreply.github.com>
2019-07-11 17:11:50 +0530

    Create middel.py

---
+++ middel.py
@@ -0,0 +1,8 @@
+sttg=list(input())
+if len(sttg)%2==0:
+    sttg[int(len(sttg)/2)] ='*'
+    sttg[int(len(sttg)/2)-1]='*'
+else:
+    sttg[int(len(sttg)/2)] ='*'
+for m in range(0,len(sttg)):
+    print(strgs[m],end='')
