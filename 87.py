ae6193f3885d0a2f3fcf1a3f06eee3a024ed57b2
baviamu <48151961+baviamu@users.noreply.github.com>
2019-07-12 17:00:56 +0530

    Create 87.py

---
+++ 87.py
@@ -0,0 +1,14 @@
+ley,maz=map(int,input().split())
+wos=[]
+ed=[]
+gcd=1
+for t in range(1,ley+1):
+    if(ley%i==0):
+        wos.append(t)
+for l in range(1,maz+1):
+    if(maz%l==0):
+        ed.append(l)
+for h in wos:
+    if h in ed:
+        gcd=gcd*h
+print(gcd)
